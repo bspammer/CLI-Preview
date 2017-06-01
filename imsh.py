15c57b97e7160db48b1122527043be66b0c3ace5
Charlie Collard <charliecollard0@gmail.com>
2017-06-01 15:41:28 +0100

    Added feature to continually redraw the image

--- imsh.py
+++ imsh.py
@@ -27,6 +27,8 @@ parser.add_argument("-n", "--noborder", dest="border", action="store_false",
         default=True, help="Disable border around output")
 parser.add_argument("-r", "--resize", dest="resize", type=float,
         help="Percentage to resize the image by")
+parser.add_argument("-w", "--watch", dest="watch", action="store_true",
+        default=False, help="Continually watch a file, redrawing changes")
 args = parser.parse_args()
 
 if args.detail == "low":
@@ -38,58 +40,66 @@ def to_console_color_str(r, g, b):
     return "\x1b[48;2;%d;%d;%dm \x1b[0m" % (r, g, b)
 
 if __name__ == "__main__":
-    for filename in args.filenames:
-        try:
-            img = Image.open(filename)
-        except IOError:
-            sys.stderr.write("Error: File '%s' does not exist\n" % filename)
-            continue
-        if args.resize:
-            divisor = 100. / args.resize
-            img = img.resize((int(img.width/divisor), int(img.height/divisor)), Image.BILINEAR)
-        px = img.getdata()
+    keep_drawing = True
+    while keep_drawing:
+        for filename in args.filenames:
+            try:
+                img = Image.open(filename)
+            except IOError:
+                sys.stderr.write("Error: File '%s' does not exist\n" % filename)
+                continue
+            if args.resize:
+                divisor = 100. / args.resize
+                img = img.resize((int(img.width/divisor), int(img.height/divisor)), Image.BILINEAR)
+            px = img.getdata()
 
-        width = args.width
-        height = args.height
-        if height == 0:
-            # Terminal characters are roughly half as wide as they are tall
-            height = int((width * img.height/img.width)/2)
+            width = args.width
+            height = args.height
+            if height == 0:
+                # Terminal characters are roughly half as wide as they are tall
+                height = int((width * img.height/img.width)/2)
 
-        if img.width < width or img.height < height:
-            sys.stderr.write("Error: Image too small")
-            exit()
+            if img.width < width or img.height < height:
+                sys.stderr.write("Error: Image too small\n")
+                exit()
 
-        out_string = ""
-        for y in range(height):
-            out_string += VERTICAL*args.border
-            for x in range(width):
-                top_left_region_x = int(x*img.width/width)
-                top_left_region_y = int(y*img.height/height)
-                bottom_right_region_x = min(int((x+1)*img.width/width), img.width-1)
-                bottom_right_region_y = min(int((y+1)*img.height/height), img.height-1)
-                intensities = []
-                for img_x in range(top_left_region_x, bottom_right_region_x):
-                    for img_y in range(top_left_region_y, bottom_right_region_y):
-                        pixel = px[img_x + img_y*img.width]
-                        if args.color:
-                            intensity = pixel
-                        else:
-                            intensity = sum(pixel)/3.
-                        intensities.append(intensity)
-                if args.color:
-                    r_list = [i[0] for i in intensities]
-                    g_list = [i[1] for i in intensities]
-                    b_list = [i[2] for i in intensities]
-                    r = int(min(255., sum(r_list)/len(r_list)))
-                    g = int(min(255., sum(g_list)/len(g_list)))
-                    b = int(min(255., sum(b_list)/len(b_list)))
-                    out_string += to_console_color_str(r, g, b)
-                else:
-                    avg_intensity = (sum(intensities)/len(intensities))/255.
-                    intensity_index = min(len(intensity_chrs)-1, int(avg_intensity*len(intensity_chrs)))
-                    out_string += intensity_chrs[intensity_index]
-            out_string += VERTICAL*args.border + "\n"
-        out_string = (TOP_LEFT + HORIZONTAL*(width) + TOP_RIGHT + "\n")*args.border \
-        + out_string \
-        + (BOT_LEFT + HORIZONTAL*(width) + BOT_RIGHT)*args.border
-        sys.stdout.write((out_string + "\n").encode("utf-8"))
+            out_string = ""
+            for y in range(height):
+                out_string += VERTICAL*args.border
+                for x in range(width):
+                    top_left_region_x = int(x*img.width/width)
+                    top_left_region_y = int(y*img.height/height)
+                    bottom_right_region_x = min(int((x+1)*img.width/width), img.width-1)
+                    bottom_right_region_y = min(int((y+1)*img.height/height), img.height-1)
+                    intensities = []
+                    for img_x in range(top_left_region_x, bottom_right_region_x):
+                        for img_y in range(top_left_region_y, bottom_right_region_y):
+                            pixel = px[img_x + img_y*img.width]
+                            if args.color:
+                                intensity = pixel
+                            else:
+                                intensity = sum(pixel)/3.
+                            intensities.append(intensity)
+                    if args.color:
+                        r_list = [i[0] for i in intensities]
+                        g_list = [i[1] for i in intensities]
+                        b_list = [i[2] for i in intensities]
+                        r = int(min(255., sum(r_list)/len(r_list)))
+                        g = int(min(255., sum(g_list)/len(g_list)))
+                        b = int(min(255., sum(b_list)/len(b_list)))
+                        out_string += to_console_color_str(r, g, b)
+                    else:
+                        avg_intensity = (sum(intensities)/len(intensities))/255.
+                        intensity_index = min(len(intensity_chrs)-1, int(avg_intensity*len(intensity_chrs)))
+                        out_string += intensity_chrs[intensity_index]
+                out_string += VERTICAL*args.border + "\n"
+            out_string = (TOP_LEFT + HORIZONTAL*(width) + TOP_RIGHT + "\n")*args.border \
+            + out_string \
+            + (BOT_LEFT + HORIZONTAL*(width) + BOT_RIGHT)*args.border \
+            + "\n"
+            if not args.watch:
+                sys.stdout.write(out_string.encode("utf-8"))
+                keep_drawing = False
+            else:
+                print(len(out_string))
+                #sys.stdout.write((out_string + "\b"*len(out_string)).encode("utf-8"))
